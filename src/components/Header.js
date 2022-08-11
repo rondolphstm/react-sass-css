a40691921e7c6a97b92fda258b3d3c73251a3ae2
rondolphstm <rondolphtst.m@gmail.com>
2022-08-11 17:39:57 -0400

    first react sass project "

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+const Header = () => {
+  return (
+    <>
+      <ul className="navigation">
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">About</a>
+        </li>
+        <li>
+          <a href="">Contact</a>
+        </li>
+        <li>
+          <a href="">Links</a>
+        </li>
+      </ul>
+      <div className="menu">
+        <a href="" className="menu__link">link 1</a>
+        <a href="" className="menu__link">link 2</a>
+        <a href="" className="menu__link">link 3</a>
+      </div>
+    </>
+  )
+}
+
+export default Header
